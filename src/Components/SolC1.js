2809c6139531dc5032e16e727a0ff311fa77f146
Ashokjpr <meenaashokkumar260@gmail.com>
2025-11-24 13:06:21 +0530

    Refactor SolC1 component structure for improved readability and layout; enhance title positioning and styling.

--- src/Components/SolC1.js
+++ src/Components/SolC1.js
@@ -3,22 +3,21 @@ import Solc1bgpc from '../assets/images/solc1bgpc.png'
 import Solc1bgmob from '../assets/images/solc1bgmob.png'
 
 function SolC1(props) {
-  return (
+    return (
         <div>
-            <div className='d-flex justify-content-center '>
-                <img src={Solc1bgpc} className='w-100 d-none d-md-block'  />
-                <img src={Solc1bgmob} className='w-100  d-md-none'  />      
-                <div className='container position-absolute d-flex justify-content-start text-light  py-auto '>
-                    <div className='pt-5   mt-5  col-11 col-md-9'>
-                        <p className=' text-2xl md:text-4xl mt-md-5 fw-bold  ' data-aos="fade-up-left">{props.title}</p>
+            <div>
+                <div className="position-relative ">
+                    <img src={Solc1bgpc} className="w-100 d-none d-md-block" />
+                    <img src={Solc1bgmob} className="w-100 d-md-none" />
+                    <div className="container position-absolute top-50 start-50 translate-middle w-100 text-center text-light">
+                        <p className="fw-bold text-2xl lg:text-4xl  text-md-3xl col-12 col-md-9 " data-aos="fade-up-left"> {props.title} </p>
                     </div>
-                   
+                </div>
+                <div className="container fs-4 my-5 text-center" data-aos="fade-down">
+                    <p>{props.description}</p>
                 </div>
             </div>
-            <div className='container fs-4  my-5 text-center'data-aos="fade-down">
-                <p>{props.description}</p>
-                {/* <p>Each solution is designed for scalability, security, and long-term performance, with full-stack architecture built for your business needs.</p>     */}
-            </div>
+
         </div>
     )
 }
